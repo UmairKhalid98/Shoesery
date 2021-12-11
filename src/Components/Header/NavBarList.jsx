45608d445f9ec2350e0d98aac70e777d436658a5
Umair <mumairkhalid30@gmail.com>
2021-12-11 14:13:31 -0800

    navbar

--- src/Components/Header/NavBarList.jsx
+++ src/Components/Header/NavBarList.jsx
@@ -1,50 +1,50 @@
 // import React, from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-import { changeFilter } from '../../features/filters';
+import { useSelector, useDispatch } from "react-redux";
+import { changeFilter } from "../../features/filters";
+import { Link } from "react-router-dom";
 
-const Supplier = ({supplier}) =>{
-
-    const filter =  useSelector((state)=> state.filter.value);
-    const dispatch = useDispatch();
-    const buttonStyle = {
-        backgroundColor:'rgba(0, 0, 0, 0.0)',
-        color:supplier.supplier_name === filter.currentFilter?'red':'white',
-        border:'none'
-        
-    }
-    return(
-        <button style = {buttonStyle}
-         onClick={()=>dispatch(changeFilter({currentFilter:supplier.supplier_name})) }
-
-         >
+const Supplier = ({ supplier }) => {
+  const filter = useSelector((state) => state.filter.value);
+  const dispatch = useDispatch();
+  const buttonStyle = {
+    backgroundColor: "rgba(0, 0, 0, 0.0)",
+    color: supplier.supplier_name === filter.currentFilter ? "red" : "white",
+    border: "none",
+  };
+  return (
+    <Link to="/">
+      <button
+        style={buttonStyle}
+        onClick={() => {
+          dispatch(changeFilter({ currentFilter: supplier.supplier_name }));
+        }}
+      >
         <h2>{supplier.supplier_name}</h2>
-        </button>
-    )
-}
+      </button>
+    </Link>
+  );
+};
 
-const NavBarList = ({supplierList}) => {
-    return (
-        <div style ={{backgroundColor:"black"}}>
-      <div style = {navStyle}>
+const NavBarList = ({ supplierList }) => {
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      <div style={navStyle}>
         {supplierList.map((supplier) => {
-          return <Supplier supplier={supplier} key={supplier.supplier_id}/>;
+          return <Supplier supplier={supplier} key={supplier.supplier_id} />;
         })}
       </div>
-      </div>
-    );
-}
+    </div>
+  );
+};
 
 const navStyle = {
-    display:"flex",
-    flexDirection: "row",
-    justifyContent:"space-evenly",
-    width: "80%",
-    margin:"auto",
-    backgroundColor:"black",
-    color:"white"
-    
-    };
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-evenly",
+  width: "80%",
+  margin: "auto",
+  backgroundColor: "black",
+  color: "white",
+};
 
-
-
-export default NavBarList
+export default NavBarList;
